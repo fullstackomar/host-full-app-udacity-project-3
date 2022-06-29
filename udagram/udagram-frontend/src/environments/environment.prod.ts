8db007f9857514ad5fc08f1f07c084f5b033297d
fullstackomar <108020780+fullstackomar@users.noreply.github.com>
2022-06-29 04:28:38 +0200

    url

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "udagram-server-dev.eba-74d7zs2w.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-server-dev.eba-74d7zs2w.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
