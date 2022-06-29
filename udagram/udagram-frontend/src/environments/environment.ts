cf8f401721e9429e9ddb2063d64cf4d022465c2e
fullstackomar <108020780+fullstackomar@users.noreply.github.com>
2022-06-29 03:09:27 +0200

    Update environment.ts

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'udagram-server-dev.eba-74d7zs2w.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
